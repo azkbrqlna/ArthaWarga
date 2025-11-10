fa9fd754796eb9c21a2b5d3b8d6e74b9cb9b6f3d
cahyogadhang <gadhang.joy@gmail.com>
2025-11-10 23:35:01 +0700

    feat: card dashboard superadmin

--- resources/js/pages/Dashboard.jsx
+++ resources/js/pages/Dashboard.jsx
@@ -27,6 +27,9 @@ export default function Dashboard() {
         saldoAwal,
         sisaSaldo,
         totalPengeluaran,
+        userTotal,
+        sisaIuran,
+        sisaBop,
     } = usePage().props;
     const userRole = auth?.user?.role_id;
 
@@ -178,46 +181,102 @@ export default function Dashboard() {
 
                     <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
                         <div className="flex flex-col md:flex-row gap-4 w-full">
-                            <div className="flex-1 bg-white border rounded-xl p-4 flex items-center gap-3">
-                                <div className="bg-gray-100 p-2 rounded-lg">
-                                    <Banknote className="w-5 h-5 text-gray-600" />
-                                </div>
-                                <div>
-                                    <p className="text-xs text-gray-500 font-medium">
-                                        Total Pemasukan
-                                    </p>
-                                    <p className="text-lg font-semibold text-gray-900">
-                                        {formatRupiah(saldoAwal)}
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="flex-1 bg-white border rounded-xl p-4 flex items-center gap-3">
-                                <div className="bg-gray-100 p-2 rounded-lg">
-                                    <Clock className="w-5 h-5 text-gray-600" />
-                                </div>
-                                <div>
-                                    <p className="text-xs text-gray-500 font-medium">
-                                        Total Pengeluaran
-                                    </p>
-                                    <p className="text-lg font-semibold text-gray-900">
-                                        {formatRupiah(totalPengeluaran)}
-                                    </p>
-                                </div>
-                            </div>
+                            {(userRole === 1) && (
+                                <div className="flex-1 bg-white border rounded-xl p-4 flex items-center gap-3">
+                                    <div className="bg-gray-100 p-2 rounded-lg">
+                                        <Banknote className="w-5 h-5 text-gray-600" />
+                                    </div>
 
-                            <div className="flex-1 bg-white border rounded-xl p-4 flex items-center gap-3">
-                                <div className="bg-gray-100 p-2 rounded-lg">
-                                    <Calculator className="w-5 h-5 text-gray-600" />
+                                    <div>
+                                        <p className="text-xs text-gray-500 font-medium">
+                                            Total KK
+                                        </p>
+                                        <p className="text-lg font-semibold text-gray-900">
+                                            {userTotal}
+                                        </p>
+                                    </div>
                                 </div>
-                                <div>
-                                    <p className="text-xs text-gray-500 font-medium">
-                                        Saldo Sekarang
-                                    </p>
-                                    <p className="text-lg font-semibold text-gray-900">
-                                        {formatRupiah(sisaSaldo)}
-                                    </p>
-                                </div>
-                            </div>
+                            )}
+                            {(userRole === 1) && (
+                            <><div className="flex-1 bg-white border rounded-xl p-4 flex items-center gap-3">
+                                    <div className="bg-gray-100 p-2 rounded-lg">
+                                        <Banknote className="w-5 h-5 text-gray-600" />
+                                    </div>
+
+                                    <div>
+                                        <p className="text-xs text-gray-500 font-medium">
+                                            Dana BOP Sekarang
+                                        </p>
+                                        <p className="text-lg font-semibold text-gray-900">
+                                            {formatRupiah(sisaBop)}
+                                        </p>
+                                    </div>
+                                </div><div className="flex-1 bg-white border rounded-xl p-4 flex items-center gap-3">
+                                        <div className="bg-gray-100 p-2 rounded-lg">
+                                            <Clock className="w-5 h-5 text-gray-600" />
+                                        </div>
+                                        <div>
+                                            <p className="text-xs text-gray-500 font-medium">
+                                                Dana Iuran Sekarang
+                                            </p>
+                                            <p className="text-lg font-semibold text-gray-900">
+                                                {formatRupiah(sisaIuran)}
+                                            </p>
+                                        </div>
+                                    </div><div className="flex-1 bg-white border rounded-xl p-4 flex items-center gap-3">
+                                        <div className="bg-gray-100 p-2 rounded-lg">
+                                            <Calculator className="w-5 h-5 text-gray-600" />
+                                        </div>
+                                        <div>
+                                            <p className="text-xs text-gray-500 font-medium">
+                                                Total Keseluruhan
+                                            </p>
+                                            <p className="text-lg font-semibold text-gray-900">
+                                                {formatRupiah(sisaSaldo)}
+                                            </p>
+                                        </div>
+                                    </div></>
+                            )}
+                            {(userRole === 2 || userRole === 3 || userRole === 4 || userRole === 5) && (
+                            <><div className="flex-1 bg-white border rounded-xl p-4 flex items-center gap-3">
+                                    <div className="bg-gray-100 p-2 rounded-lg">
+                                        <Banknote className="w-5 h-5 text-gray-600" />
+                                    </div>
+
+                                    <div>
+                                        <p className="text-xs text-gray-500 font-medium">
+                                            Total Pemasukan
+                                        </p>
+                                        <p className="text-lg font-semibold text-gray-900">
+                                            {formatRupiah(saldoAwal)}
+                                        </p>
+                                    </div>
+                                </div><div className="flex-1 bg-white border rounded-xl p-4 flex items-center gap-3">
+                                        <div className="bg-gray-100 p-2 rounded-lg">
+                                            <Clock className="w-5 h-5 text-gray-600" />
+                                        </div>
+                                        <div>
+                                            <p className="text-xs text-gray-500 font-medium">
+                                                Total Pengeluaran
+                                            </p>
+                                            <p className="text-lg font-semibold text-gray-900">
+                                                {formatRupiah(totalPengeluaran)}
+                                            </p>
+                                        </div>
+                                    </div><div className="flex-1 bg-white border rounded-xl p-4 flex items-center gap-3">
+                                        <div className="bg-gray-100 p-2 rounded-lg">
+                                            <Calculator className="w-5 h-5 text-gray-600" />
+                                        </div>
+                                        <div>
+                                            <p className="text-xs text-gray-500 font-medium">
+                                                Saldo Sekarang
+                                            </p>
+                                            <p className="text-lg font-semibold text-gray-900">
+                                                {formatRupiah(sisaSaldo)}
+                                            </p>
+                                        </div>
+                                    </div></>
+                            )}
                         </div>
                     </div>
                 </div>
