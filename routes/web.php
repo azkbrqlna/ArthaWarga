88ef71e779b88ec7039cacdef7c21f4c5c09d83b
azkbrqlna <azkbrq@gmail.com>
2025-11-16 20:06:46 +0700

    feat: add API for Auth and BOP

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BopController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IuranController;
